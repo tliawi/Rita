86e8a06e09358b864975d0b7f32dd58baf3a46b3
tliawi <jhn.frfld@gmail.com>
2022-01-11 21:19:52 -0500

    Update rita.js

--- rita.js
+++ rita.js
@@ -13,8 +13,11 @@ open index.html in the Chrome browser.
 function rita(imgCanvas) {
 
 var imgContext = imgCanvas.getContext("2d");
-imgContext.globalAlpha = 1; //no transparency  
-var imgData = imgContext.getImageData(0, 0, imgCanvas.width, imgCanvas.height); //is a copy
+imgContext.globalAlpha = 1; //no transparency 
+var height = imgCanvas.height;
+var width = imgCanvas.width;
+    
+var imgData = imgContext.getImageData(0, 0, width, height); //is a copy
 var rgba = imgData.data; //can modify this copy, and subsequently display with an imgContext.putImageData
     
 // Two dimensional structures (rgba and cellauto) are indexed in row major order (i.e. row, column) 
@@ -22,15 +25,15 @@ var rgba = imgData.data; //can modify this copy, and subsequently display with a
 // positive going down (in rows) and to the right (in cols).
     
 
-function allocate2d(){
-    var y = new Array(imgContext.height);
-    for (let i=0;i<imgContext.height;i++) y[i] = (new Array(imgContext.width)).fill(0);
+function allocate2d(v){
+    var y = new Array(height);
+    for (let i=0;i<height;i++) y[i] = (new Array(width)).fill(v);
     return y;
 }
 
 function clearRGBA(){
-    for (let i=0;i<imgCanvas.height;i++) for (let j=0;imgCanvas.width;j++)  {
-            let ix = 4*(i*imgCanvas.width + j);
+    for (let i=0;i<height;i++) for (let j=0;j<width;j++)  {
+            let ix = 4*(i*width + j);
             rgba[ix] = 0;
             rgba[ix+1] = 0;
             rgba[ix+2] = 0;
@@ -39,13 +42,17 @@ function clearRGBA(){
     imgContext.putImageData(imgData, 0, 0);
 }
 
+    
+function magnitude(numArr){ //numArr a one dimensional numeric array
+    return Math.max(Math.max(...numArr), -Math.min(...numArr));
+}
+
 // clobbers rgba, a global r,g,b,alpha linear canvas data array visualizing the numeric data 
-// in a given complete two dimensional numeric Array
+// in a given height by width numeric array (like um,u,up)
 // Numeric values are rendered in a spectrum 
 // from green (positive) to grey (zero) to red (negative).
 function renderToRGBA(numArr, threshold=9999999999){
     const GREYVAL = 64;
-    var height = numArr.length, width = numArr[0].length;
     
     // find greatest magnitude, to normalize with
     var mag = 0;
@@ -79,11 +86,11 @@ function renderToRGBA(numArr, threshold=9999999999){
     imgContext.putImageData(imgData, 0, 0);
 }
 
-um = allocate2d(); //u minus, prior time step
-u  = allocate2d(); //current
-up = allocate2d(); //u plus, next time step, calculated as fcn of u and um
+um = allocate2d(0.0); //u minus, prior time step
+u  = allocate2d(0.0); //current
+up = allocate2d(0.0); //u plus, next time step, calculated as fcn of u and um
     
-meta = allocate2d(); //determines the function to be applied at that pixel
+meta = allocate2d(0); //determines the function to be applied at that pixel
     
 //current velocity = (u - um)/T, where T is time it takes to do iteration, here taken as 1
 
@@ -96,28 +103,30 @@ function advanceT(){
 
 
 function computeUp(){
-    maxi = u.length - 1;
-    maxj = u[0].length - 1;
+    var maxi = height - 1;
+    var maxj = width - 1;
     
     //four corners
-    up[0][0]       = (u[1][0]+u[0][1]) - um[0][0];
-    up[maxi][maxj] = (u[maxi][maxj-1]+u[maxi-1][maxj]) - um[maxi][maxj];
-    up[0][maxj]    = (u[0][maxj-1]+u[1][maxj]) - um[0,maxj];
-    up[maxi][0]    = (u[maxi-1][0]+u[maxi][1]) - um[maxi][0];
+    up[0][0]       = (u[1][0]+u[0][1])/2.0                  - um[0][0];
+    up[maxi][maxj] = (u[maxi][maxj-1]+u[maxi-1][maxj])/2.0  - um[maxi][maxj];
+    up[0][maxj]    = (u[0][maxj-1]+u[1][maxj])/2.0          - um[0][maxj];
+    up[maxi][0]    = (u[maxi-1][0]+u[maxi][1])/2.0          - um[maxi][0];
     
     //four edges
-    for (int i=1;i<maxi;i++) { 
-        up[i][0] = (u[i-1][0]+u[i+1][0]+u[i,1]) - 2*um[i][0];
-        up[i][maxj] = (u[i-1][maxj]+u[i+1][maxj]+u[i][maxj-1]) - 2*um[i][maxj];
+    for (let i=1;i<maxi;i++) { 
+        up[i][0] = (u[i-1][0]+u[i+1][0]+u[i][1])/3.0                - um[i][0];
+        up[i][maxj] = (u[i-1][maxj]+u[i+1][maxj]+u[i][maxj-1])/3.0  - um[i][maxj];
     }
-    for (int j=1;j<maxj;j++){
-        up[0][j] = (u[0][j-1]+u[0][j+1]+u[1,j]) - 2*um[0][j];
-        up[maxi][j] = (u[maxi][j-1]+u[maxi][j+1]+u[maxi-1][j]) - 2*um[maxi][j];
+    for (let j=1;j<maxj;j++){
+        up[0][j] = (u[0][j-1]+u[0][j+1]+u[1][j])/3.0                - um[0][j];
+        up[maxi][j] = (u[maxi][j-1]+u[maxi][j+1]+u[maxi-1][j])/3.0  - um[maxi][j];
     }
     
     //core
-    for (int i=1;i<maxi;i++) for (int j=1;j<maxj;j++){
-        up[i][j] = (u[i-1][j]+u[i+1][j]+u[i][j-1]+u[i][j+1]) - 3*um[i][j];
+    for (let i=1;i<maxi;i++) { 
+        for (let j=1;j<maxj;j++){
+            up[i][j] = (u[i-1][j]+u[i+1][j]+u[i][j-1]+u[i][j+1])/4.0 - um[i][j];
+        }
     }
     
 }  
@@ -131,8 +140,9 @@ function step(n=1){
 }
     
 return { 
-        clearImage: clearImage,
         clearRGBA: clearRGBA,
+        imgContext:imgContext,
+        imgData:imgData,
         renderToRGBA: renderToRGBA,
         step: step,
         um: um,
